b36496a44a19086fc934f42a0b2a79a3e024f07e
kankan-web <2728360489@qq.com>
2025-12-06 13:34:34 +0800

    feat: 项目基础结构实现

---
+++ constant.ts
@@ -0,0 +1,40 @@
+
+// 首页信息
+export const HomeInfo = {
+  title: "阿娟蛋的Blog",
+  description: "阿娟蛋的个人博客，专注于技术分享与生活记录。基于Astor构建，简洁快速，支持深色模式",
+}
+
+// 四零四信息
+export const NotFoundInfo = {
+  title: "404 Not Found",
+  description: "抱歉，您访问的页面不存在或者已经被移动",
+}
+
+// 导航站信息
+export const NavigationInfo = {
+  title: "导航站",
+  description: "各类网站导航，方便快捷",
+}
+
+// 时间轴信息
+export const TimelineInfo = {
+  title: "时间轴",
+  description: "时间轴，记录阿娟蛋的成长历程",
+}
+
+// 项目信息
+export const ProjectInfo = {
+  title: "项目",
+  description: "项目，记录阿娟蛋的开发历程",
+}
+
+export const Links = [
+  {url: '/', name: '首页&简介'},
+  {url: '/navigation', name: '导航站'},
+  {url: '/notes', name: '笔记'},
+  {url: '/posts', name: '文章'},
+  {url: '/timeline', name: '时间轴'},
+  {url: '/projects', name: '项目'},
+  {url: '/friends', name: '友链'},
+]
